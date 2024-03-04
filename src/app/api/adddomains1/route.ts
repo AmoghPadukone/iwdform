829e6c830e26f0697909205312fb3ce30fdc6533
Amogh <amoghpadukone@gmail.com>
2024-03-04 16:05:26 +0530

    fix2

--- src/app/api/adddomains1/route.ts
+++ src/app/api/adddomains1/route.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
     const prisma = new PrismaClient();
     const badges = ['aiml', 'fullstack', 'datascience', 'data analytics'];
     const createdBadges = [];
